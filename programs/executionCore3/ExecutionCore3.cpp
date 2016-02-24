143f7f42589b537b73adfd7faa5f0143e92b037a
jgvictores <jcgvicto@gmail.com>
2016-02-24 19:41:12 +0100

    desarrollo de exec3 para control de cabeza por velocidad

---
+++ programs/executionCore3/ExecutionCore3.cpp
@@ -0,0 +1,84 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "ExecutionCore3.hpp"
+#include "InCvPort.hpp"
+
+namespace teo
+{
+
+/************************************************************************/
+
+bool ExecutionCore3::configure(ResourceFinder &rf) {
+
+    //ConstString fileName(DEFAULT_FILE_NAME);
+    
+    printf("--------------------------------------------------------------\n");
+    if (rf.check("help")) {
+        printf("ExecutionCore3 options:\n");
+        printf("\t--help (this help)\t--from [file.ini]\t--context [path]\n");
+        //printf("\t--file (default: \"%s\")\n",fileName.c_str());
+    }
+    //if (rf.check("file")) fileName = rf.find("file").asString();
+    //printf("ExecutionCore1 using file: %s\n",fileName.c_str());
+
+    printf("--------------------------------------------------------------\n");
+    if(rf.check("help")) {
+        ::exit(1);
+    }
+
+    //
+    Property headOptions;
+    headOptions.put("device","remote_controlboard");
+    headOptions.put("local","/executionCore3/head");
+    headOptions.put("remote","/teo/head");
+    headDevice.open(headOptions);
+    if( ! headDevice.isValid() ) {
+        printf("head remote_controlboard instantiation not worked.\n");
+        return false;
+    }
+
+    //yarp::dev::IVelocityControl *iVelocityControl;
+    if( ! headDevice.view(iVelocityControl) ) {
+        printf("view(iVelocityControl) not worked.\n");
+        return false;
+    }
+    inCvPort.setIVelocityControl(iVelocityControl);
+//    iVelocityControl->setVelocityMode();
+//    inCvPort.setIVelocityControl(iVelocityControl);
+    //-----------------OPEN LOCAL PORTS------------//
+ //   inSrPort.setInCvPortPtr(&inCvPort);
+    inCvPort.useCallback();
+ //   inSrPort.useCallback();
+ //   inSrPort.open("/executionCore3/dialogueManager/command:i");
+    inCvPort.open("/executionCore3/cv/state:i");
+
+    return true;
+}
+
+/************************************************************************/
+double ExecutionCore3::getPeriod() {
+    return 2.0;  // Fixed, in seconds, the slow thread that calls updateModule below
+}
+
+/************************************************************************/
+bool ExecutionCore3::updateModule() {
+    //printf("StateMachine in state [%d]. ExecutionCore1 alive...\n", stateMachine.getMachineState());
+    return true;
+}
+
+/************************************************************************/
+
+bool ExecutionCore3::interruptModule() {
+    printf("ExecutionCore3 closing...\n");
+    inCvPort.disableCallback();
+//    inSrPort.disableCallback();
+    inCvPort.interrupt();
+//    inSrPort.interrupt();
+    inCvPort.close();
+//    inSrPort.close();
+    return true;
+}
+
+/************************************************************************/
+
+}  // namespace teo
---
+++ programs/executionCore3/InCvPort.cpp
@@ -0,0 +1,111 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "InCvPort.hpp"
+
+namespace teo
+{
+
+/************************************************************************/
+
+void InCvPort::onRead(Bottle& b) {
+//    if ( ! follow ) return;
+    if (b.size() < 3) return;
+
+    double x = b.get(0).asDouble();
+    double y = b.get(1).asDouble();
+    double z = b.get(2).asDouble();
+
+/*    double x1, x2, x3, x4;
+    double y1, y2, y3, y4;
+
+    x4 = x3;
+    x3 = x2;
+    x2 = x1;
+    x1 = x;
+
+    y4 = y3;
+    y3 = y2;
+    y2 = y1;
+    y1 = y;
+
+    x_tmp = ((x1 + x2 + x3 + x4) / 4);
+    y_tmp = ((y1 + y2 + y3 + y4) / 4); 
+
+    double x_tmp, y_tmp,;
+*/
+
+    printf("%f %f %f\n",x,y,z);
+
+    if( x > -150 ) {
+        iVelocityControl->velocityMove(0, 5.0); //Id motor. Velocity [deg/s].
+    }
+    else if( x > -170 && x < -150) {
+        iVelocityControl->velocityMove(0, 2.0);
+    }
+    else if( x > -190 && x < -170 ) {
+        iVelocityControl->velocityMove(0, 1.0);
+    }
+    else if( x > -220 && x < -190 ) {
+        iVelocityControl->velocityMove(0, 0.0);
+    }
+    else if( x < -220 && x > -240 ) {
+        iVelocityControl->velocityMove(0, -1.0);
+    }
+    else if( x < -240 && x > -260 ) {
+        iVelocityControl->velocityMove(0, -2.0);
+    }
+    else if( x < -260 ) {
+        iVelocityControl->velocityMove(0, -5.0);
+    }
+
+    if( y > -190 ) {
+        iVelocityControl->velocityMove(0, 5.0); //Id motor. Velocity [deg/s].
+    }
+    else if( y > -210 && x < -190) {
+        iVelocityControl->velocityMove(0, 2.0);
+    }
+    else if( y > -230 && x < -210 ) {
+        iVelocityControl->velocityMove(0, 1.0);
+    }
+    else if( y > -270 && x < -220 ) {
+        iVelocityControl->velocityMove(0, 0.0);
+    }
+    else if( y < -270 && x > -290 ) {
+        iVelocityControl->velocityMove(0, -1.0);
+    }
+    else if( y < -290 && x > -310 ) {
+        iVelocityControl->velocityMove(0, -2.0);
+    }
+    else if( y < -310 ) {
+        iVelocityControl->velocityMove(0, -5.0);
+    }
+
+    /*
+    if( x > -150 ) iPositionControl->relativeMove(0, 4);
+    if( x > -170 ) iPositionControl->relativeMove(0, 2);
+    if( x > -190 ) iPositionControl->relativeMove(0, 1);
+    if( x < -220 ) iPositionControl->relativeMove(0, -1);
+    if( x < -240 ) iPositionControl->relativeMove(0, -2);
+    if( x < -260 ) iPositionControl->relativeMove(0, -4);
+    //
+    if( y > -190 ) iPositionControl->relativeMove(1, 4);
+    if( y > -210 ) iPositionControl->relativeMove(1, 2);
+    if( y > -230 ) iPositionControl->relativeMove(1, 1);
+    if( y < -270 ) iPositionControl->relativeMove(1, -1);
+    if( y < -290 ) iPositionControl->relativeMove(1, -2);
+    if( y < -310 ) iPositionControl->relativeMove(1, -4);
+*/
+
+}
+
+/************************************************************************/
+
+void InCvPort::setFollow(bool value)
+{
+    follow = value;
+}
+
+/************************************************************************/
+
+}  // namespace teo
+
